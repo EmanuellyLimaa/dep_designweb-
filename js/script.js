842de013f5265911acfe02ef462ec643fa2478c6
Emanuelly Maria <emanuellylima1422@gmail.com>
2026-08-26 15:58:47 -0300

    sintaxe  js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("oi do externo");
+alert("oi do externo"); 
+
+var num = 2;
+if (num % 2 == 0){
+    alert(`${num} é par`);
+}else if (num %2 != 0){
+    alert(`${num} é impar`);
+}else{
+    alert(`${num} é zero`);
+}
+
+alert("Fim");
