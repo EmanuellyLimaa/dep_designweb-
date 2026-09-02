01aecea21a3276f8ac8edc711230acb0c4e41f55
Emanuelly Maria <emanuellylima1422@gmail.com>
2026-09-02 15:12:03 -0300

    nova variável

--- js/script.js
+++ js/script.js
@@ -13,10 +13,22 @@ let altura;
 function calcIdade(ano = 2026){
     let idade = ano - nasc;
     console.log(`Idade dentro da funcao: ${idade}`);
-    return idade;
+    let menor;
+    if(idade <18){
+        let menor = true;
+    }else{
+        let menor = false;
+    }
+    return [idade, menor];
 }
 
-alert(`Idade fora da funcao: ${calcIdade()}`);
+let retorno = calcIdade()
+if (retorno[1]){
+    alert(`Idade: ${retorno[0]}. Você é menor de idade`);
+}else{
+    alert(`Idade: ${retorno[0]}. Você é maior de idade`);
+}
+
 /*
 dá erro pois idade não existe fora do escopo locla dela (bloco calcIdade)
 console.log(`Idade fora da função: ${idade}`)
