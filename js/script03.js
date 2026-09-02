6bdd00d4d5ad63cefb0d0d1e629e83ece61a6398
Emanuelly Maria <emanuellylima1422@gmail.com>
2026-09-02 16:12:58 -0300

    atualizado

--- js/script03.js
+++ js/script03.js
@@ -18,6 +18,6 @@ console.log ("imc", typeof(imc));
 
 let msg = `Nome: ${nome}\nAno de Nascimento: ${nasc}\nViva: ${viva}`;
 msg = + `\nAltura: ${altura}\nSexo: ${sexo}\nPeso: ${peso}`;
-msg = msg + `\nIMC: ${imc.toExponential(2)}`;
+msg = msg + `\nIMC: ${imc.toFixed(2)}`;
 
 alert(msg);
