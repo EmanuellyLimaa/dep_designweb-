a34e11c4c8cda301198612fe487dbf2f64696b10
Emanuelly Maria <emanuellylima1422@gmail.com>
2026-09-09 14:21:15 -0300

    estrutura de seleção

---
+++ js/script03.js
@@ -0,0 +1,19 @@
+let nasc = prompt("Digite seu ano de nascimento: ");
+nasc = Number(nasc);
+
+idade = 2026 - nasc;
+
+let fds = confirm("Clique em ok se hoje for final de semana. ")
+
+alert(`Você é maior de idade: ${idade >=18}`);
+alert(`Hoje é final de semana: ${fds}`);
+
+if (idade >= 18 && fds){
+    alert(`Pode beber`);
+}else if(idade<18 && fds){
+    alert(`Não pode beber, pois menor de idade`);
+}else if(idade >=18 && !fds){
+    alert(`não pode beber, pois não é final de semana`);
+}else{
+    lert(`Não pode beber, pois não é menor de idade e não é final de semana`);
+}
